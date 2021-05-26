2c1abf63ffc2ee6360d19f61351bb42d3f5f6f57
ebasanez <enrique@darwinex.com>
2021-05-26 06:41:56 +0200

    Basic Flux operations

--- src/main/java/es/bprojects/coures/webflux/Application.java
+++ src/main/java/es/bprojects/coures/webflux/Application.java
@@ -1,11 +1,15 @@
 package es.bprojects.coures.webflux;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import es.bprojects.coures.webflux.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @author ebasanez
@@ -23,26 +27,33 @@ public class Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		// Example of flux that throws error when processing one of their elements.
-		Flux<String> nombres = Flux.just("Andrés", "Pedro", "", "Juan")
-				.map(String::toUpperCase)
+		List<String> names = List.of("Andrés Guzman", "Pedro Almodobar", "Alfonso Lopez", "Juan Salva", "pedro Sanchez");
+		Flux<User> flux = Flux.fromIterable(names)
+				.map(name -> new User(name.split(" ")[0], name.split(" ")[1]))
+				.filter(u -> "Pedro".equalsIgnoreCase(u.getName()))
+				// Otra forma de filtar, con flatMap:
+				/*
+				.flatMap(u -> {
+					if ("Pedro".equalsIgnoreCase(u.getName())) {
+						return Mono.just(u);
+					} else {
+						return Mono.empty();
+					}
+				})
+				*/
 				.doOnNext(e -> {
-					if (e.isEmpty()) {
+					if (e.getName().isEmpty()) {
 						throw new RuntimeException("Element is empty");
 					} else {
 						System.out.println(e);
 					}
 				});
 
-		// Handles error and interrupts flux processing:
-		nombres.subscribe(e-> log.info(e),
-				error -> log.error("Error: empty name"));
-
-		// No se llama si ha habido algún error.
-		nombres.doOnComplete(new Runnable() {
-			@Override
-			public void run() {
-				log.info("Flux processing finished");
-			}
-		});
+		flux.subscribe(e -> log.info("User: " + e),
+				// Handles error and interrupts flux processing:
+				error -> log.error("Error: empty name"),
+				// Executes wjhen flux is closed. Wont be invoked if there has been any error.
+				() -> log.info("Flux processing finished")
+		);
 	}
 }
